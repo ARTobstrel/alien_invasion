446c95276b0be9dc18f33d6ae27f4b2eeac8319d
artem <artemfap@yandex.ru>
2017-08-20 23:09:12 +0300

    add Class Alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -15,18 +16,20 @@ def run_game():
     )
     pygame.display.set_caption("Alien Invasion")
 
-    # Создание корабля
+    # Создание корабля.
     ship = Ship(ai_settings, screen)
 
     # Создание группы для хранения пуль.
     bullets = Group()
 
-    # Запуск оновного цикла
+    # Создание пришельца.
+    alien = Alien(ai_settings, screen)
+
+    # Запуск оновного цикла.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
