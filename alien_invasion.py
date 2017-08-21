32d3cf427b27319b8b32cc3e2ce7740d3b07d6fb
artem <artemfap@yandex.ru>
2017-08-21 23:21:42 +0300

    create aliens fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,6 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -16,20 +15,20 @@ def run_game():
     )
     pygame.display.set_caption("Alien Invasion")
 
-    # Создание корабля.
+    # Создание корабля, группы пуль и группы пришельцев.
     ship = Ship(ai_settings, screen)
-
-    # Создание группы для хранения пуль.
     bullets = Group()
+    aliens = Group()
 
-    # Создание пришельца.
-    alien = Alien(ai_settings, screen)
+    # Создание флота пришельцев.
+    gf.create_fleet(ai_settings, screen, aliens)
+
 
     # Запуск оновного цикла.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -3,6 +3,7 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагирует на нажатие клавиш."""
@@ -39,7 +40,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Обновляет изображения на экране и отображает новый экран."""
     # При каждом проходе цикла перерисовывается экран.
     screen.fill(ai_settings.bg_color)
@@ -48,7 +49,7 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # Отображение последнего прорисованного экрана.
     pygame.display.flip()
@@ -62,3 +63,20 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def create_fleet(ai_settings, screen, aliens):
+    """Создает флот пришельцев."""
+    # Создание пришельца и вычисление количества пришельцев в ряду.
+    # Интервал между соседними пришельцами равен одной ширине пришельца.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2*alien_width))
+
+    # Создание первого ряда пришельцев.
+    for alien_number in range(number_aliens_x):
+        # Создание пришельца и размещение его в ряду.
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
