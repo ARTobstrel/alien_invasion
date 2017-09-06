eb0a827f0e2b57506d61b2d470a77869e2f64357
artem <artemfap@yandex.ru>
2017-09-06 23:18:55 +0300

    add ships left images2

--- game_functions.py
+++ game_functions.py
@@ -177,25 +177,23 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets):
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Проверяет, достиг ди флот края экрана. Обновляет позиции всех пришельцев во флоте."""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # Проверка коллизий "пришелец-корабль".
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets)
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
     # Прверка пришельцевб добравшихся до нижнего края экрана.
-    check_aliens_bottom(ai_settings, stats, screen, sb, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
-def ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets):
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Обрабатывает столкновение корабля с пришельцем."""
     if stats.ships_left > 0:
         # Уменбшение ship_left.
         stats.ships_left -= 1
-        # Обновление оставшихся звездолетов
-        sb.prep_ships()
 
         # Очистка списков пришельцев и пуль.
         aliens.empty()
@@ -212,13 +210,13 @@ def ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True) # Возвращает указатель мыши
 
-def check_aliens_bottom(ai_settings, stats, screen, sb, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """Проверяет, достигли ли пришельцы нижнего края экрана."""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # Происходит то же, что при столкновении с кораблем.
-            ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets)
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
             break
 
 def check_high_score(stats, sb):
